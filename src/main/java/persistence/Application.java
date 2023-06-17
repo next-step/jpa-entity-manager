85c950725a9d6337d3ed1e108cc51c40dc4d41c6
wotjd243 <wotjd243@naver.com>
2023-06-17 19:40:04 +0900

    feat: set up the project

--- src/main/java/persistence/Application.java
+++ src/main/java/persistence/Application.java
@@ -2,13 +2,9 @@ package persistence;
 
 import database.DatabaseServer;
 import database.H2;
-import domain.Person;
 import jdbc.JdbcTemplate;
-import jdbc.PersonRowMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import persistence.sql.ddl.DDLGenerator;
-import persistence.sql.dml.DMLGenerator;
 
 public class Application {
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
@@ -20,19 +16,6 @@ public class Application {
             server.start();
 
             final JdbcTemplate jdbcTemplate = new JdbcTemplate(server.getConnection());
-
-            DDLGenerator ddlGenerator = new DDLGenerator(Person.class);
-            DMLGenerator dmlGenerator = new DMLGenerator(Person.class);
-
-            jdbcTemplate.execute(ddlGenerator.generateCreate());
-            jdbcTemplate.execute(dmlGenerator.generateInsert(new Person("name", 26, "email", 1)));
-
-            PersonRowMapper rowMapper = new PersonRowMapper();
-            jdbcTemplate.query(dmlGenerator.generateFindAll(), rowMapper);
-            Person person = jdbcTemplate.queryForObject(dmlGenerator.generateFindById(1L), rowMapper);
-            jdbcTemplate.execute(dmlGenerator.generateDelete(person));
-
-            jdbcTemplate.execute(ddlGenerator.generateDrop());
 
             server.stop();
         } catch (Exception e) {
