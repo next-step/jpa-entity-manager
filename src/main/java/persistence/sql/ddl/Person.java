5899f63bdd8b923c3c291d4088e60a9c1bf9a6e1
seondongpyo <seondongpyo@gmail.com>
2023-06-21 11:07:59 +0900

    feat: 1주차 미션 코드 이관

---
+++ src/main/java/persistence/sql/ddl/Person.java
@@ -0,0 +1,78 @@
+package persistence.sql.ddl;
+
+import jakarta.persistence.*;
+
+@Table(name = "users")
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nick_name")
+    private String name;
+
+    @Column(name = "old", length = 3)
+    private Integer age;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Transient
+    private Integer index;
+
+    public Person() {
+    }
+
+    public Person(String name, Integer age, String email) {
+        this(null, name, age, email);
+    }
+
+    public Person(Long id, String name, Integer age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getIndex() {
+        return index;
+    }
+
+    public void setIndex(Integer index) {
+        this.index = index;
+    }
+}
