3b3a97fce246ca4c9f989ddae1092c8f4c84ac0e
wooobo <12059592+wooobo@users.noreply.github.com>
2023-06-22 10:20:01 +0900

    feat: 이관 처리 (#6)

---
+++ src/main/java/domain/Person.java
@@ -0,0 +1,57 @@
+package domain;
+
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nick_name")
+    private String name;
+
+    @Column(name = "old", length = 3)
+    private Integer age;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Transient
+    private Integer index;
+
+    public Person() {
+
+    }
+
+    public Person(Long id, String name, Integer age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+
+    public Person(String name, Integer age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id) && Objects.equals(name, person.name) && Objects.equals(age, person.age) && Objects.equals(email, person.email) && Objects.equals(index, person.index);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age, email, index);
+    }
+}
