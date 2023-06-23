0cef7682484e45b52ab0f2a879521981a01ffce3
wooobo <irooori89@gmail.com>
2023-06-24 01:07:51 +0900

    refactor: `DdlType` 의 else 구문 제거

--- src/main/java/persistence/dialect/DdlType.java
+++ src/main/java/persistence/dialect/DdlType.java
@@ -18,8 +18,8 @@ public class DdlType {
         int loc = template.indexOf(placeholder);
         if (loc < 0) {
             return template;
-        } else {
-            return template.substring(0, loc) + replacement + template.substring(loc + placeholder.length());
         }
+
+        return template.substring(0, loc) + replacement + template.substring(loc + placeholder.length());
     }
 }
