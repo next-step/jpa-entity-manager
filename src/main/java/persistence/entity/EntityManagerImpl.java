eb0cb171c8532b33aa3dcace04fd7b75896c200d
seondongpyo <seondongpyo@gmail.com>
2023-06-27 22:03:40 +0900

    feat: [요구사항 2] 1차 캐시 적용

--- src/main/java/persistence/entity/EntityManagerImpl.java
+++ src/main/java/persistence/entity/EntityManagerImpl.java
@@ -14,13 +14,24 @@ import java.util.Arrays;
 public class EntityManagerImpl implements EntityManager {
 
     private final JdbcTemplate jdbcTemplate;
+    private final PersistenceContext persistenceContext;
 
     public EntityManagerImpl(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        this.persistenceContext = createPersistenceContext();
+    }
+
+    private PersistenceContext createPersistenceContext() {
+        return new PersistenceContextImpl();
     }
 
     @Override
     public <T> T find(Class<T> clazz, Long id) {
+        Object entity = persistenceContext.getEntity(id);
+        if (entity != null) {
+            return (T) entity;
+        }
+
         SelectQueryBuilder builder = new SelectQueryBuilder(clazz);
         String query = builder.buildFindByIdQuery(id);
         return jdbcTemplate.queryForObject(query, new EntityRowMapper<>(clazz));
@@ -31,6 +42,33 @@ public class EntityManagerImpl implements EntityManager {
         InsertQueryBuilder builder = new InsertQueryBuilder(entity.getClass());
         String query = builder.build(entity);
         jdbcTemplate.execute(query);
+        addEntityToPersistenceContext(entity);
+    }
+
+    private void addEntityToPersistenceContext(Object entity) {
+        Class<?> entityClass = entity.getClass();
+        SelectQueryBuilder builder = new SelectQueryBuilder(entityClass);
+        String query = builder.buildFindLastQuery();
+        Object entityWithId = jdbcTemplate.queryForObject(query, new EntityRowMapper<>(entityClass));
+        Long id = findPrimaryKey(entityWithId);
+        persistenceContext.addEntity(id, entityWithId);
+    }
+
+    private Long findPrimaryKey(Object entity) {
+        try {
+            Field idField = getIdField(entity.getClass());
+            idField.setAccessible(true);
+            return (Long) idField.get(entity);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Field getIdField(Class<?> entityClass) {
+        return Arrays.stream(entityClass.getDeclaredFields())
+            .filter(field -> field.isAnnotationPresent(Id.class))
+            .findFirst()
+            .orElseThrow(() -> new NoIdentifierException(entityClass.getSimpleName()));
     }
 
     @Override
@@ -39,18 +77,6 @@ public class EntityManagerImpl implements EntityManager {
         DeleteQueryBuilder builder = new DeleteQueryBuilder(entity.getClass());
         String query = builder.buildDeleteByIdQuery(id);
         jdbcTemplate.execute(query);
-    }
-
-    private Long findPrimaryKey(Object entity) {
-        try {
-            Field idField = Arrays.stream(entity.getClass().getDeclaredFields())
-                .filter(field -> field.isAnnotationPresent(Id.class))
-                .findFirst()
-                .orElseThrow(() -> new NoIdentifierException(entity.getClass().getSimpleName()));
-            idField.setAccessible(true);
-            return (Long) idField.get(entity);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
+        persistenceContext.removeEntity(id);
     }
 }
