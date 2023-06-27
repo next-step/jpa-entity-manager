e24fbe2a65d847a07376e66de89fd0eea30564b2
seondongpyo <seondongpyo@gmail.com>
2023-06-27 22:29:49 +0900

    refactor: 테스트 메서드 분리

--- src/test/java/persistence/entity/PersistenceContextTest.java
+++ src/test/java/persistence/entity/PersistenceContextTest.java
@@ -1,5 +1,6 @@
 package persistence.entity;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import persistence.sql.ddl.Person;
@@ -9,24 +10,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 class PersistenceContextTest {
 
     private final PersistenceContext persistenceContext = new PersistenceContextImpl();
+    private static final Long PERSON_JACK_ID = 1L;
+    private static final Person PERSON_JACK = new Person(PERSON_JACK_ID, "jack", 20, "jack@abc.com");
+
+    @BeforeEach
+    void init() {
+        persistenceContext.addEntity(PERSON_JACK_ID, PERSON_JACK);
+    }
+
+    @DisplayName("영속성 컨텍스트에서 엔티티를 조회한다.")
+    @Test
+    void getEntity() {
+        Object entity = persistenceContext.getEntity(PERSON_JACK_ID);
+        assertThat(entity).isEqualTo(PERSON_JACK);
+    }
 
     @DisplayName("영속성 컨텍스트에 엔티티를 저장한다.")
     @Test
     void addEntity() {
-        Long personId = 1L;
-        Person person = new Person(personId, "jack", 20, "jack@abc.com");
-        persistenceContext.addEntity(personId, person);
-        Object entity = persistenceContext.getEntity(personId);
+        Person person = new Person(2L, "kevin", 30, "kevin@abc.com");
+        persistenceContext.addEntity(person.getId(), person);
+        Object entity = persistenceContext.getEntity(person.getId());
         assertThat(entity).isEqualTo(person);
     }
 
     @DisplayName("영속성 컨텍스트에서 엔티티를 제거한다.")
     @Test
     void removeEntity() {
-        Long personId = 1L;
-        Person person = new Person(personId, "jack", 20, "jack@abc.com");
-        persistenceContext.addEntity(personId, person);
-        persistenceContext.removeEntity(personId);
-        assertThat(persistenceContext.getEntity(personId)).isNull();
+        persistenceContext.removeEntity(PERSON_JACK_ID);
+        assertThat(persistenceContext.getEntity(PERSON_JACK_ID)).isNull();
     }
 }
