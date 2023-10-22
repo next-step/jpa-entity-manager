56ff260c5b2ec20228ea5ffe91f8d22e839cbf16
Zinzo <42106799+zinzoddari@users.noreply.github.com>
2023-10-22 20:10:14 +0900

    feat: 1주차 미션 가져오기 (#24)

---
+++ src/main/java/persistence/sql/ddl/CreateQuery.java
@@ -0,0 +1,32 @@
+package persistence.sql.ddl;
+
+import persistence.sql.common.meta.EntityMeta;
+
+class CreateQuery extends EntityMeta {
+
+    private static final String DEFAULT_CREATE_QUERY = "CREATE TABLE %s (%s)";
+    private static final String DEFAULT_PRIMARY_KEY_QUERY = ", PRIMARY KEY (%s)";
+
+    private <T> CreateQuery(Class<T> tClass) {
+        super(tClass);
+    }
+
+    public static <T> String create(Class<T> tClass) {
+        return new CreateQuery(tClass).join();
+    }
+
+    private String join() {
+        return String.join(", ", combineQuery());
+    }
+
+    /**
+     * 해당 Class를 분석하여 CREATE QUERY로 조합합니다.
+     */
+    private String combineQuery() {
+        return String.format(DEFAULT_CREATE_QUERY, getTableName(), getConstraintsWithColumns() + createColumns());
+    }
+
+    private String createColumns() {
+        return String.format(DEFAULT_PRIMARY_KEY_QUERY, getPrimaryKeyWithComma());
+    }
+}
