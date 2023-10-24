f4feeee62175f02ff82b0318573418355d04cbdc
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-10-24 21:18:59 +0900

    refactor: EntityClass 의존성 제거

--- src/main/java/hibernate/entity/EntityPersister.java
+++ src/main/java/hibernate/entity/EntityPersister.java
@@ -3,13 +3,19 @@ package hibernate.entity;
 import hibernate.dml.DeleteQueryBuilder;
 import hibernate.dml.InsertQueryBuilder;
 import hibernate.dml.UpdateQueryBuilder;
+import hibernate.entity.column.EntityColumn;
+import hibernate.entity.column.EntityColumns;
 import jdbc.JdbcTemplate;
+
+import java.util.AbstractMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class EntityPersister<T> {
 
-    private final EntityClass<T> entityClass;
     private final EntityTableName tableName;
-    private final Class<T> clazz;
+    private final EntityColumns entityColumns;
     private final JdbcTemplate jdbcTemplate;
     private final InsertQueryBuilder insertQueryBuilder = InsertQueryBuilder.INSTANCE;
     private final DeleteQueryBuilder deleteQueryBuilder = DeleteQueryBuilder.INSTANCE;
@@ -17,17 +23,17 @@ public class EntityPersister<T> {
 
     public EntityPersister(final Class<T> clazz, final JdbcTemplate jdbcTemplate) {
         this.tableName = new EntityTableName(clazz);
-        this.clazz = clazz;
-        this.entityClass = new EntityClass<>(clazz);
+        this.entityColumns = new EntityColumns(clazz.getDeclaredFields());
         this.jdbcTemplate = jdbcTemplate;
     }
 
     public boolean update(final Object entity) {
+        EntityColumn entityId = entityColumns.getEntityId();
         final String query = updateQueryBuilder.generateQuery(
                 tableName.getTableName(),
-                entityClass.getFieldValues(entity),
-                entityClass.getEntityId(),
-                entityClass.extractEntityId(entity)
+                getFieldValues(entity),
+                entityId,
+                entityId.getFieldValue(entity)
         );
         return jdbcTemplate.executeUpdate(query);
     }
@@ -35,17 +41,31 @@ public class EntityPersister<T> {
     public void insert(final Object entity) {
         final String query = insertQueryBuilder.generateQuery(
                 tableName.getTableName(),
-                entityClass.getFieldValues(entity)
+                getFieldValues(entity)
         );
         jdbcTemplate.execute(query);
     }
 
     public void delete(final Object entity) {
+        EntityColumn entityId = entityColumns.getEntityId();
         final String query = deleteQueryBuilder.generateQuery(
                 tableName.getTableName(),
-                entityClass.getEntityId(),
-                entityClass.extractEntityId(entity)
+                entityId,
+                entityId.getFieldValue(entity)
         );
         jdbcTemplate.execute(query);
+    }
+
+    private Map<EntityColumn, Object> getFieldValues(final Object entity) {
+        return entityColumns.getValues()
+                .stream()
+                .map(entityColumn -> new AbstractMap.SimpleEntry<>(entityColumn, entityColumn.getFieldValue(entity)))
+                .filter(entry -> entry.getValue() != null)
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (existing, replacement) -> existing,
+                        LinkedHashMap::new
+                ));
     }
 }
