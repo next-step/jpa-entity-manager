ed163c0f0bc9b715fcd43682f2b2c446eba3c119
hhhhhwi <aab555586@gmail.com>
2023-10-27 01:37:17 +0900

    feat: 요구사항 1 - EntityPersister 구현
    - UpdateQueryBuilder 구현
    - Entity를 DB에 매핑 및 쿼리 실행의 책임을 갖는 EntityPersister 구현

---
+++ src/test/java/persistence/sql/dml/UpdateQueryBuilderTest.java
@@ -0,0 +1,35 @@
+package persistence.sql.dml;
+
+import domain.Person;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import persistence.sql.metadata.EntityMetadata;
+import persistence.sql.metadata.EntityValue;
+import persistence.sql.metadata.EntityValues;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UpdateQueryBuilderTest {
+	private UpdateQueryBuilder updateQueryBuilder = new UpdateQueryBuilder();
+
+	private final EntityMetadata entityMetadata = new EntityMetadata(Person.class);
+
+	private final Person person = new Person(1L, "update", 2, "update@email.com", 0);
+
+	@DisplayName("Person 객체로 UPDATE 쿼리 생성 테스트")
+	@Test
+	void test_buildQuery() {
+		Field[] fields = person.getClass().getDeclaredFields();
+
+		EntityValues entityValues = new EntityValues(Arrays.stream(fields)
+				.map(x -> new EntityValue(x, person))
+				.filter(EntityValue::checkPossibleToBeValue)
+				.collect(Collectors.toList()));
+
+		assertEquals(updateQueryBuilder.buildByIdQuery(entityMetadata, entityValues, new WhereClauseBuilder(person)), "UPDATE users SET nick_name = 'update', old = 2, email = 'update@email.com' WHERE id = 1;");
+	}
+}
