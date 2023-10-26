48fbcb16afd6929dab191504481414478172fc62
hhhhhwi <aab555586@gmail.com>
2023-10-27 01:35:26 +0900

    refactor: InsertQueryBuilder EntityValues 주입으로 구조 변경
    - 등록하려는 객체를 외부에서 만들어서 주입하도록 수정

--- src/main/java/persistence/sql/dml/InsertQueryBuilder.java
+++ src/main/java/persistence/sql/dml/InsertQueryBuilder.java
@@ -1,12 +1,7 @@
 package persistence.sql.dml;
 
 import persistence.sql.metadata.EntityMetadata;
-import persistence.sql.metadata.Value;
-import persistence.sql.metadata.Values;
-
-import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import persistence.sql.metadata.EntityValues;
 
 import static java.lang.String.format;
 
@@ -16,17 +11,11 @@ public class InsertQueryBuilder {
     public InsertQueryBuilder() {
     }
 
-    public String buildQuery(EntityMetadata entityMetadata, Object insertObject) {
-        if(insertObject == null) {
+    public String buildQuery(EntityMetadata entityMetadata, EntityValues entityValues) {
+        if(entityValues == null) {
             throw new IllegalArgumentException("등록하려는 객체가 NULL 값이 될 수 없습니다.");
         }
 
-        Field[] fields = insertObject.getClass().getDeclaredFields();
-
-        Values values = new Values(Arrays.stream(fields)
-                .map(x -> new Value(x, insertObject))
-                .filter(Value::checkPossibleToInsert)
-                .collect(Collectors.toList()));
-        return format(INSERT_COMMAND, entityMetadata.getTableName(), values.buildColumnsClause(), "(" +  values.buildValueClause() + ")");
+        return format(INSERT_COMMAND, entityMetadata.getTableName(), entityValues.buildColumnsClause(), "(" +  entityValues.buildValueClause() + ")");
     }
 }
--- src/test/java/persistence/sql/dml/InsertQueryBuilderTest.java
+++ src/test/java/persistence/sql/dml/InsertQueryBuilderTest.java
@@ -4,6 +4,12 @@ import domain.Person;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import persistence.sql.metadata.EntityMetadata;
+import persistence.sql.metadata.EntityValue;
+import persistence.sql.metadata.EntityValues;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,8 +21,15 @@ class InsertQueryBuilderTest {
 	@DisplayName("Person 객체로 INSERT 쿼리 생성 테스트")
 	@Test
 	void test_buildQuery() {
+		Field[] fields = person.getClass().getDeclaredFields();
+
+		EntityValues entityValues = new EntityValues(Arrays.stream(fields)
+				.map(x -> new EntityValue(x, person))
+				.filter(EntityValue::checkPossibleToBeValue)
+				.collect(Collectors.toList()));
+
 		assertEquals(
-				new InsertQueryBuilder().buildQuery(entityMetadata, person),
+				new InsertQueryBuilder().buildQuery(entityMetadata, entityValues),
 				"INSERT INTO users (nick_name, old, email) VALUES ('hhhhhwi',1,'aab555586@gmail.com');"
 		);
 	}
