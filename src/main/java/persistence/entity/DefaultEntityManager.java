52b415d93f71848b4ab42ed0316f6df3b75a8d30
limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>
2023-10-26 13:37:28 +0900

    refactor) DefaultEntityManager 의 find 세부기능을 EntityLoader 클래스로 위임

--- src/main/java/persistence/entity/DefaultEntityManager.java
+++ src/main/java/persistence/entity/DefaultEntityManager.java
@@ -1,32 +1,26 @@
 package persistence.entity;
 
-import jdbc.EntityRowMapper;
 import jdbc.JdbcTemplate;
-import persistence.sql.dialect.Dialect;
-import persistence.sql.dialect.DialectFactory;
-import persistence.sql.dialect.h2.H2Dialect;
-import persistence.sql.dml.DmlQueryGenerator;
 
 public class DefaultEntityManager implements EntityManager {
 
     private final JdbcTemplate jdbcTemplate;
     private final EntityPersister entityPersister;
+    private final EntityLoader entityLoader;
 
-    private DefaultEntityManager(JdbcTemplate jdbcTemplate, EntityPersister entityPersister) {
+    private DefaultEntityManager(JdbcTemplate jdbcTemplate, EntityPersister entityPersister, EntityLoader entityLoader) {
         this.jdbcTemplate = jdbcTemplate;
         this.entityPersister = entityPersister;
+        this.entityLoader = entityLoader;
     }
 
     public static DefaultEntityManager of(JdbcTemplate jdbcTemplate) {
-        return new DefaultEntityManager(jdbcTemplate, EntityPersister.of(jdbcTemplate));
+        return new DefaultEntityManager(jdbcTemplate, EntityPersister.of(jdbcTemplate), EntityLoader.of(jdbcTemplate));
     }
 
     @Override
     public <T> T find(Class<T> clazz, Long id) {
-        // TODO : step 2-2 에서 EntityLoader 를 통해 구현
-        DmlQueryGenerator dmlQueryGenerator = DmlQueryGenerator.of(H2Dialect.getInstance());
-        String selectByPkQuery = dmlQueryGenerator.generateSelectByPkQuery(clazz, id);
-        return jdbcTemplate.queryForObject(selectByPkQuery, new EntityRowMapper<>(clazz));
+        return entityLoader.find(clazz, id);
     }
 
     @Override
