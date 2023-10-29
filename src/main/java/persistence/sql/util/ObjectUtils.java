cb99f52db309ec0abfdd849ef40f03310137eb91
limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>
2023-10-30 01:36:33 +0900

    fix) 생성자 접근권한해제

--- src/main/java/persistence/sql/util/ObjectUtils.java
+++ src/main/java/persistence/sql/util/ObjectUtils.java
@@ -1,5 +1,6 @@
 package persistence.sql.util;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 
 public class ObjectUtils {
@@ -8,7 +9,9 @@ public class ObjectUtils {
         try {
             Class<?> clazz = source.getClass();
 
-            Object target = clazz.getDeclaredConstructor().newInstance();
+            Constructor<?> constructor = clazz.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            Object target = constructor.newInstance();
 
             for (Field field : clazz.getDeclaredFields()) {
                 field.setAccessible(true);
@@ -16,6 +19,7 @@ public class ObjectUtils {
             }
             return target;
         } catch (Exception e) {
+            System.out.println(e);
             throw new IllegalStateException("엔티티 복제 도중 오류가 발생하였습니다");
         }
     }
