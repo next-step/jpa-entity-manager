58599610ba4b8c2a6565b170183f84ce9e2085d6
limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>
2023-10-29 17:55:03 +0900

    test) hasAutoGenId 테스트 작성

--- src/test/java/persistence/sql/meta/ColumnMetasTest.java
+++ src/test/java/persistence/sql/meta/ColumnMetasTest.java
@@ -23,4 +23,11 @@ class ColumnMetasTest {
         ColumnMetas idColumns = columnMetas.idColumns();
         assertThat(idColumns.getColumnsClause()).isEqualTo("id");
     }
+
+    @Test
+    @DisplayName("엔티티 컬럼 중 자동생성 ID 컬럼이 존재하는 경우 참을 반환한다")
+    void hasAutoGenId() {
+        ColumnMetas columnMetas = ColumnMetas.of(Person.class.getDeclaredFields());
+        assertThat(columnMetas.hasAutoGenId()).isTrue();
+    }
 }
