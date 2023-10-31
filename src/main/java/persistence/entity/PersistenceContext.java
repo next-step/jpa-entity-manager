eabe27c8a7a8655bacd8144d2b34563dd5daea18
KeyNam <rlfbd5142@gmail.com>
2023-10-31 17:59:13 +0900

    feat : PersistenceContext 구현

---
+++ src/main/java/persistence/entity/PersistenceContext.java
@@ -0,0 +1,20 @@
+package persistence.entity;
+
+import java.util.Optional;
+import persistence.entity.impl.SnapShot;
+
+public interface PersistenceContext {
+    Optional<Object> getEntity(Class<?> entityClazz, Object id);
+
+    void addEntity(Object id, Object entity);
+
+    void removeEntity(Object entity);
+
+    Object getDatabaseSnapshot(Object id, Object entity);
+
+    SnapShot getSnapShot(Class<?> entityClazz, Object id);
+
+    void purgeEntityCache(Object entity);
+
+    void clearContextCache();
+}
