d57d287a815dcbc77841a24f0587e29a8cae4068
profoundsea25 <profoundsea25@gmail.com>
2023-11-04 15:36:37 +0900

    fixup: 테스트 코드 정상 작동하도록 수정
    - SimpleEntityManager: select 결과가 없으면 null 반환, delete시 PersistenceContext에서도 삭제
    - EntityPersisterTest: EntityManager 대신 EntityLoader 사용

--- src/main/java/persistence/entity/SimpleEntityManager.java
+++ src/main/java/persistence/entity/SimpleEntityManager.java
@@ -22,12 +22,15 @@ public class SimpleEntityManager implements EntityManager {
     @Override
     public <T, K> T find(Class<T> clazz, K id) {
         Object cached = persistenceContext.getEntity(new EntityKey(clazz, id));
-
         if (cached != null) {
             return (T) cached;
         }
 
         T selected = entityLoader.selectById(clazz, id);
+        if (selected == null) {
+            return null;
+        }
+
         persistenceContext.addEntity(new EntityKey(selected), selected);
         persistenceContext.getDatabaseSnapshot(new EntityKey(selected), selected);
         return selected;
@@ -59,6 +62,7 @@ public class SimpleEntityManager implements EntityManager {
     @Override
     public void remove(Object entity) {
         entityPersister.delete(entity);
+        persistenceContext.removeEntity(new EntityKey(entity));
     }
 
 }
