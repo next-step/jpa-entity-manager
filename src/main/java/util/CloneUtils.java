cceadcc3f7142bb8eb4b993c7c6ed04b18cb1f0a
Jivebread <spark@troy.edu>
2023-11-09 22:05:33 +0900

    feature(CloneUtils): snapshot을 위한 deepcopy를 진행할때 필요한 deepcopy를 진행했습니다.

---
+++ src/main/java/util/CloneUtils.java
@@ -0,0 +1,37 @@
+package util;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
+public class CloneUtils {
+
+  public static Object clone(Object entity) {
+    try {
+      Class<?> clazz = entity.getClass();
+      Constructor<?> constructor = getConstructor(clazz);
+
+      Object cloned = constructor.newInstance();
+
+      Field[] fields = clazz.getDeclaredFields();
+
+      for (Field field : fields) {
+        field.setAccessible(true);
+        Object value = field.get(entity);
+        field.set(cloned, value);
+      }
+      return cloned;
+
+    } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+             NoSuchMethodException e) {
+      throw new RuntimeException("생성자 오류", e);
+    }
+  }
+
+  private static Constructor<?> getConstructor(Class<?> clazz) throws NoSuchMethodException {
+
+    Constructor<?> constructor = clazz.getDeclaredConstructor();
+    constructor.setAccessible(true);
+    return constructor;
+  }
+}
