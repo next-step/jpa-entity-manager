8e30f64c33113b7f70cdecaaa892589844db6f5b
Hyunta <mohai2618@gmail.com>
2024-02-26 20:07:46 +0900

    feat: id가 String일 경우 따옴표를 감싸주도록 수정

--- src/main/java/persistence/sql/dml/SelectQueryBuilder.java
+++ src/main/java/persistence/sql/dml/SelectQueryBuilder.java
@@ -1,6 +1,8 @@
 package persistence.sql.dml;
 
 import persistence.sql.domain.Column;
+import persistence.sql.domain.DataType;
+import persistence.sql.domain.IdColumn;
 import persistence.sql.domain.Table;
 
 import java.util.List;
@@ -8,7 +10,7 @@ import java.util.stream.Collectors;
 
 public class SelectQueryBuilder {
     private static final String SELECT_QUERY_TEMPLATE = "SELECT %s FROM %s";
-    private static final String WHERE_CLAUSE_TEMPLATE = " WHERE %s = %d";
+    private static final String WHERE_CLAUSE_TEMPLATE = " WHERE %s = %s";
     private static final String COLUMN_DELIMITER = ", ";
 
     private static class InstanceHolder {
@@ -35,7 +37,16 @@ public class SelectQueryBuilder {
     }
 
     private String whereClause(Table table, Object id) {
-        String name = table.getIdColumn().getName();
-        return String.format(WHERE_CLAUSE_TEMPLATE, name, id);
+        IdColumn idColumn = table.getIdColumn();
+        String value = getDmlValue(id, idColumn);
+        return String.format(WHERE_CLAUSE_TEMPLATE, idColumn.getName(), value);
+    }
+
+    private String getDmlValue(Object id, Column column) {
+        DataType columnType = column.getType();
+        if (columnType.isVarchar()) {
+            return String.format("'%s'", id);
+        }
+        return id.toString();
     }
 }
