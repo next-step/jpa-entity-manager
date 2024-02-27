4e51714b1cf801c79e7b0c9ea6a5db57300955f3
csbiy <katd6@naver.com>
2024-02-27 23:54:58 +0900

    test : Update 도메인 객체에 대한 unit 테스트 추가

---
+++ src/test/java/persistence/sql/domain/UpdateTest.java
@@ -0,0 +1,20 @@
+package persistence.sql.domain;
+
+import org.junit.jupiter.api.Test;
+import persistence.sql.entity.Person;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UpdateTest {
+
+    @Test
+    void should_return_update_set_clause() {
+        Person person = new Person(1l,"cs", 29, "katd216@gmail.com", 0);
+        DatabaseTable table = new DatabaseTable(person);
+
+        String updateClause = new Update(table)
+                .getUpdateClause();
+
+        assertThat(updateClause).isEqualTo("nick_name='cs',old=29,email='katd216@gmail.com'");
+    }
+}
