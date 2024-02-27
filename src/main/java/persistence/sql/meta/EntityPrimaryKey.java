a332bd7e7e281afef8c504cc9d99d75f8f9d3f59
qwe5507 <apvmffkdls@gmail.com>
2024-02-28 00:34:30 +0900

    refactor: [step1피드백반영] PrimaryKey 단일 객체로 변경

--- src/main/java/persistence/sql/meta/EntityPrimaryKey.java
+++ src/main/java/persistence/sql/meta/EntityPrimaryKey.java
@@ -2,51 +2,40 @@ package persistence.sql.meta;
 
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Transient;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class EntityPrimaryKey {
-    private List<EntityColumn> entityColumns;
+    private EntityColumn primaryKey;
     private EntityPrimaryKey(final Class<?> clazz) {
-        this.entityColumns = Arrays.stream(clazz.getDeclaredFields())
-                .filter(this::isNotTransientField)
+        this.primaryKey = Arrays.stream(clazz.getDeclaredFields())
                 .filter(this::isIdField)
+                .findFirst()
                 .map(EntityColumn::new)
-                .collect(Collectors.toList());
+                .orElseThrow(IllegalArgumentException::new);
     }
     public static EntityPrimaryKey of(Class<?> clazz) {
         return new EntityPrimaryKey(clazz);
     }
 
-    // 현재는 PK가 한개만 있다고 가정
     public String name() {
-        return this.entityColumns.get(0).getFieldName();
+        return this.primaryKey.getFieldName();
     }
 
     public String value(final Object object) {
-        return this.entityColumns.get(0).value(object);
-    }
-
-    public EntityColumn getEntityColumn() {
-        return entityColumns.get(0);
+        return this.primaryKey.value(object);
     }
 
     public Class<?> type() {
-        return this.entityColumns.get(0).type();
+        return this.primaryKey.type();
     }
+
     public GenerationType strategy() {
-        return this.entityColumns.get(0).generateType();
+        return this.primaryKey.generateType();
     }
 
     private boolean isIdField(Field field) {
         return field.isAnnotationPresent(Id.class);
-    }
-
-    private boolean isNotTransientField(final Field field) {
-        return !field.isAnnotationPresent(Transient.class);
     }
 }
