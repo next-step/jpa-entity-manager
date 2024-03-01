a34a5eedce95426e15bc1fc513f8cdcdc01c2cf2
csbiy <katd6@naver.com>
2024-03-01 23:43:53 +0900

    refactor : setEntityId 비즈니스로직 이동

--- src/main/java/persistence/entity/EntityPersister.java
+++ src/main/java/persistence/entity/EntityPersister.java
@@ -4,15 +4,13 @@ import jdbc.JdbcTemplate;
 import persistence.sql.dml.DeleteQueryBuild;
 import persistence.sql.dml.InsertQueryBuild;
 import persistence.sql.dml.UpdateQueryBuild;
-import persistence.sql.domain.DatabasePrimaryColumn;
-import persistence.sql.domain.DatabaseTable;
 import persistence.sql.domain.Query;
-
-import java.lang.reflect.Field;
 
 public class EntityPersister {
 
     private final JdbcTemplate jdbcTemplate;
+
+    private final EntityInformation entityInformation;
 
     private final InsertQueryBuild insertQueryBuilder;
 
@@ -22,6 +20,7 @@ public class EntityPersister {
 
     public EntityPersister(JdbcTemplate jdbcTemplate, InsertQueryBuild insertQueryBuilder, UpdateQueryBuild updateQueryBuilder, DeleteQueryBuild deleteQueryBuilder) {
         this.jdbcTemplate = jdbcTemplate;
+        this.entityInformation = new EntityInformation();
         this.insertQueryBuilder = insertQueryBuilder;
         this.updateQueryBuilder = updateQueryBuilder;
         this.deleteQueryBuilder = deleteQueryBuilder;
@@ -38,27 +37,14 @@ public class EntityPersister {
     }
 
     public void insert(Object entity) {
-        DatabasePrimaryColumn primaryColumn = new DatabaseTable(entity).getPrimaryColumn();
         Query query = insertQueryBuilder.insert(entity);
-        if (primaryColumn.hasColumnValue()) {
-            jdbcTemplate.execute(query.getSql());
+        if (entityInformation.isNew(entity)) {
+            Long id = jdbcTemplate.executeAndReturnGeneratedKey(query.getSql());
+            entityInformation.setEntityId(entity, id);
             return;
         }
-        Long id = jdbcTemplate.executeAndReturnGeneratedKey(query.getSql());
-        setEntityId(entity, id);
+        executeQuery(query);
     }
-
-    private void setEntityId(Object entity, Object id) {
-        DatabasePrimaryColumn primaryColumn = new DatabaseTable(entity).getPrimaryColumn();
-        try {
-            Field declaredField = entity.getClass().getDeclaredField(primaryColumn.getJavaFieldName());
-            declaredField.setAccessible(true);
-            declaredField.set(entity, id);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 
     public void delete(Object entity) {
         Query query = deleteQueryBuilder.delete(entity);
--- src/test/java/persistence/entity/EntityInformationTest.java
+++ src/test/java/persistence/entity/EntityInformationTest.java
@@ -1,6 +1,7 @@
 package persistence.entity;
 
 import org.junit.jupiter.api.Test;
+import persistence.sql.domain.DatabaseTable;
 import persistence.sql.entity.Person;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,5 +17,16 @@ class EntityInformationTest {
                 () -> assertThat(entityInformation.isNew(new Person("cs", 29, "katd216@gmail.com", 1))).isTrue(),
                 () -> assertThat(entityInformation.isNew(new Person(1L, "cs", 29, "katd216@gmail.com", 1))).isFalse()
         );
+    }
+
+    @Test
+    void should_set_entity_id() {
+        Long id = 1L;
+        Person person = new Person("cs", 29, "katd216@gmail.com", 1);
+
+        entityInformation.setEntityId(person, id);
+        String entityId = new DatabaseTable(person).getPrimaryColumn().getColumnValue();
+
+        assertThat(entityId).isEqualTo(String.valueOf(id));
     }
 }
