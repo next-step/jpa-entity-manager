86e4e38e9f1cf43283ae0d7ecc839e38143049f8
Shin seongsu <tlstjdtn321@naver.com>
2024-03-01 20:15:18 +0900

    test: update 테스트 코드 추가

---
+++ src/test/java/persistence/sql/dml/query/builder/UpdateQueryBuilderTest.java
@@ -0,0 +1,37 @@
+package persistence.sql.dml.query.builder;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import persistence.entity.Person;
+import persistence.sql.dml.conditional.Criteria;
+import persistence.sql.dml.conditional.Criterion;
+import persistence.sql.entity.EntityMappingTable;
+import persistence.sql.entity.model.DomainType;
+
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UpdateQueryBuilderTest {
+    private Person person;
+    private EntityMappingTable entityMappingTable;
+    private DomainType pkDomainType;
+
+    @BeforeEach
+    void setUp() {
+        this.person = new Person(1L, "박재성", 20, "jason@nextstep.com");
+        this.entityMappingTable = EntityMappingTable.of(Person.class, person);
+        this.pkDomainType = entityMappingTable.getPkDomainTypes();
+    }
+
+    @Test
+    void updateQueryTest() {
+        Criterion criterion = Criterion.of(pkDomainType.getColumnName(), pkDomainType.getValue().toString());
+        Criteria criteria = new Criteria(Collections.singletonList(criterion));
+
+        UpdateQueryBuilder queryBuilder = UpdateQueryBuilder.of(entityMappingTable, criteria);
+
+        assertThat(queryBuilder.toSql()).isEqualTo("UPDATE Person SET id=1,nick_name='박재성',old=20,email='jason@nextstep.com' WHERE where id='1'");
+    }
+
+}
