2c8c7406cdf3cbac00a799258eedaae7a1b2233d
who-is-hu <dydgndlaek97@gmail.com>
2024-03-01 22:45:22 +0900

    fix: -1 하지 않아도됨
    테스트 잘못짜서 -1 해줬던 것

--- src/main/java/persistence/entity/H2GeneratedIdObtainStrategy.java
+++ src/main/java/persistence/entity/H2GeneratedIdObtainStrategy.java
@@ -6,15 +6,11 @@ import jdbc.RowMapper;
 public class H2GeneratedIdObtainStrategy implements GeneratedIdObtainStrategy {
     @Override
     public Long getGeneratedId(JdbcTemplate jdbcTemplate) {
-        Long nextId = jdbcTemplate.queryForObject("CALL IDENTITY()", getNextId());
-        return getCurrentId(nextId);
+        return jdbcTemplate.queryForObject("CALL IDENTITY()", getNextId());
     }
 
     private RowMapper<Long> getNextId() {
         return rs -> rs.getLong(1);
     }
 
-    private long getCurrentId(Long nextId) {
-        return nextId - 1;
-    }
 }
