22f645da9f7a2729c6702d0838671b0e562f8b42
ckyeon <qsky8520@gmail.com>
2024-03-02 17:02:17 +0900

    refactor(SimplePersistenceContextTest): 중복 코드 정리

--- src/test/java/persistence/entity/SimplePersistenceContextTest.java
+++ src/test/java/persistence/entity/SimplePersistenceContextTest.java
@@ -6,17 +6,15 @@ import org.junit.jupiter.api.Test;
 import persistence.study.sql.ddl.Person3;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class SimplePersistenceContextTest {
 
     private final PersistenceContext persistenceContext = new SimplePersistenceContext();
+    private final Person3 person = new Person3(1L, "qwer1", 123, "email1@email.com");
 
     @BeforeEach
     void setUp() {
-        Person3 person1 = new Person3(1L, "qwer1", 123, "email1@email.com");
-
-        persistenceContext.addEntity(1L, person1);
+        persistenceContext.addEntity(1L, person);
     }
 
     @DisplayName("1차 캐시에서 엔티티를 정상적으로 가져온다.")
@@ -24,22 +22,23 @@ class SimplePersistenceContextTest {
     void getEntity() {
         Object result = persistenceContext.getEntity(1L);
 
-        Person3 expect = new Person3(1L, "qwer1", 123, "email1@email.com");
-        assertThat(result).isEqualTo(expect);
+        assertThat(result).isEqualTo(person);
     }
 
     @DisplayName("엔티티를 persistence context의 1차 캐시에 추가할 수 있다.")
     @Test
     void addEntity() {
-        Person3 person = new Person3(1L, "qwer", 123, "email@email.com");
+        Person3 person = new Person3(2L, "qwer", 123, "email@email.com");
 
-        assertDoesNotThrow(() -> persistenceContext.addEntity(1L, person));
+        persistenceContext.addEntity(2L, person);
+
+        Object result = persistenceContext.getEntity(2L);
+        assertThat(result).isEqualTo(person);
     }
 
     @DisplayName("1차 캐시에서 엔티티가 정상적으로 제거된다.")
     @Test
     void removeEntity() {
-        Person3 person = new Person3(1L, "qwer1", 123, "email1@email.com");
         persistenceContext.removeEntity(person);
 
         Object result = persistenceContext.getEntity(1L);
@@ -49,10 +48,10 @@ class SimplePersistenceContextTest {
     @DisplayName("캐시에 넣은 객체와 꺼낸 객체의 동일성이 보장된다.")
     @Test
     void identity() {
-        Person3 person = new Person3(1L, "qwer1", 123, "email1@email.com");
+        Person3 person = new Person3(2L, "qwer1", 123, "email1@email.com");
 
-        persistenceContext.addEntity(1L, person);
-        Object result = persistenceContext.getEntity(1L);
+        persistenceContext.addEntity(2L, person);
+        Object result = persistenceContext.getEntity(2L);
 
         assertThat(person == result).isTrue();
     }
