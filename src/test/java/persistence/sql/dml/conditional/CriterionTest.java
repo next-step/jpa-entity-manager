632f1f2f3a4a43252c62c336c25c1987fb5c944b
Shin seongsu <tlstjdtn321@naver.com>
2024-03-03 03:42:59 +0900

    test: 조건문 테스트 코드 추가

---
+++ src/test/java/persistence/sql/dml/conditional/CriterionTest.java
@@ -0,0 +1,27 @@
+package persistence.sql.dml.conditional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import persistence.sql.entity.model.Operators;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CriterionTest {
+
+    @DisplayName("조건문 저장한 값을 반환한다.")
+    @Test
+    void customCriterion() {
+        Criterion criterion = new Criterion("id", "nextstep", Operators.NOT_EQUALS);
+
+        assertThat(criterion.toSql()).isEqualTo("id!='nextstep'");
+    }
+
+    @DisplayName("동일한 값을 체크하는 조건문을 반환한다.")
+    @Test
+    void equlasCriterion() {
+        Criterion criterion = Criterion.of("id", "123");
+
+        assertThat(criterion.toSql()).isEqualTo("id='123'");
+    }
+
+}
