d80e0165bc495afcfbd8397646722e8488e111b2
ckyeon <qsky8520@gmail.com>
2024-03-04 19:45:29 +0900

    feat(SimpleEntityManager): 더티체크 기능 추가

--- src/main/java/persistence/entity/SimpleEntityManger.java
+++ src/main/java/persistence/entity/SimpleEntityManger.java
@@ -1,6 +1,10 @@
 package persistence.entity;
 
 import jakarta.persistence.EntityExistsException;
+import persistence.sql.model.PKColumn;
+import persistence.sql.model.Table;
+
+import java.util.Objects;
 
 public class SimpleEntityManger implements EntityManager {
 
@@ -31,19 +35,59 @@ public class SimpleEntityManger implements EntityManager {
         if (isExist(entity)) {
             throw new EntityExistsException();
         }
-        persister.create(entity);
+        Object id = persister.create(entity);
+        persistenceContext.addEntity((Long) id, entity);
     }
 
     @Override
     public void merge(Object entity) {
-        if (isExist(entity)) {
-            persister.update(entity);
+        if (!isExist(entity)) {
+            Object id = persister.create(entity);
+            persistenceContext.addEntity((Long) id, entity);
+            return;
         }
-        persister.create(entity);
+
+        if (isDirty(entity)) {
+            Object id = persister.update(entity);
+            persistenceContext.addEntity((Long) id, entity);
+        }
     }
 
     private boolean isExist(Object entity) {
         return persistenceContext.isCached(entity) || loader.isExist(entity);
+    }
+
+    private boolean isDirty(Object entity) {
+        Class<?> clazz = entity.getClass();
+        Table table = new Table(clazz);
+
+        Object id = getEntityId(entity);
+        Object snapshot = persistenceContext.getDatabaseSnapshot((Long) id, entity);
+        if (snapshot == null) {
+            persistenceContext.addEntity((Long) id, entity);
+            return false;
+        }
+
+        EntityBinder entityBinder = new EntityBinder(entity);
+        EntityBinder snapshotBinder = new EntityBinder(snapshot);
+
+        return table.getColumns()
+                .stream()
+                .anyMatch(column -> {
+                    Object entityValue = entityBinder.getValue(column);
+                    Object snapshotValue = snapshotBinder.getValue(column);
+                    return !Objects.equals(entityValue, snapshotValue);
+                });
+    }
+
+    private Object getEntityId(Object entity) {
+        Class<?> clazz = entity.getClass();
+        Table table = new Table(clazz);
+
+        EntityBinder entityBinder = new EntityBinder(entity);
+
+        PKColumn pkColumn = table.getPKColumn();
+        return entityBinder.getValue(pkColumn);
     }
 
     @Override
