66ac29c1a54f8bfed4062335d3d6bdf9925e997b
parkje0927 <parkje09270@gmail.com>
2024-03-04 23:06:05 +0900

    refactor: step2 - JavaMappingType 의 역할을 Dialect 에 위임하여 클래스 리팩토링

--- src/main/java/domain/dialect/H2Dialect.java
+++ src/main/java/domain/dialect/H2Dialect.java
@@ -1,20 +1,34 @@
 package domain.dialect;
 
 import java.sql.Types;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * H2 라는 방언을 결정하는 곳
  */
 public class H2Dialect extends Dialect {
 
+    private final Map<Integer, String> map = new HashMap<>();
+
     public H2Dialect() {
-        registerColumnType(Types.INTEGER, "int");
-        registerColumnType(Types.BIGINT, "bigint");
-        registerColumnType(Types.VARCHAR, "varchar");
+        registerColumnType(Types.INTEGER, Integer.class);
+        registerColumnType(Types.BIGINT, Long.class);
+        registerColumnType(Types.VARCHAR, String.class);
+
+        map.put(Types.INTEGER, "int");
+        map.put(Types.BIGINT, "bigint");
+        map.put(Types.VARCHAR, "varchar");
+    }
+
+    public String getTypeToStr(Class<?> clazz) {
+        return map.get(getJavaTypeByClass(clazz));
     }
 
     @Override
-    public String getColumnDefine(int typeCode) {
-        return super.getColumnDefine(typeCode);
+    public void checkJavaType(Integer typeCode) {
+        if (!super.containsJavaType(typeCode)) {
+            throw new IllegalStateException("지원하는 javaType 이 아닙니다.");
+        }
     }
 }
