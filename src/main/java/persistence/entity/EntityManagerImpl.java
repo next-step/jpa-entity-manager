b3af15aa624cfa4eeced71ebb59cdddc9202553f
jaesungahn91 <jaesungahn91@gmail.com>
2024-03-06 20:20:57 +0900

    feat: 요구사항2. EntityManager 의 책임 줄여주기

--- src/main/java/persistence/entity/EntityManagerImpl.java
+++ src/main/java/persistence/entity/EntityManagerImpl.java
@@ -1,28 +1,20 @@
 package persistence.entity;
 
-import jdbc.GenericRowMapper;
 import jdbc.JdbcTemplate;
-import persistence.sql.dml.DeleteQueryBuilder;
-import persistence.sql.dml.InsertQueryBuilder;
-import persistence.sql.dml.SelectQueryBuilder;
 
 public class EntityManagerImpl implements EntityManager {
 
-    private final JdbcTemplate jdbcTemplate;
     private final EntityPersister entityPersister;
     private final EntityLoader entityLoader;
 
     public EntityManagerImpl(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
         this.entityPersister = new EntityPersisterImpl(jdbcTemplate);
         this.entityLoader = new EntityLoaderImpl(jdbcTemplate);
     }
 
     @Override
     public <T> T find(Class<T> clazz, Long Id) {
-        SelectQueryBuilder selectQueryBuilder = new SelectQueryBuilder(clazz, Id);
-        return jdbcTemplate.queryForObject(selectQueryBuilder.build(),
-                resultSet -> new GenericRowMapper<T>(clazz).mapRow(resultSet));
+        return entityLoader.find(clazz, Id);
     }
 
     @Override
