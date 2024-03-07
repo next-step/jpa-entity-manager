1caa2148efd78c312514ed8944f842fb102b20ff
Shin seongsu <tlstjdtn321@naver.com>
2024-03-07 20:49:50 +0900

    test: Persistence 테스트 코드 추가

---
+++ src/test/java/persistence/sql/entity/context/PersistenceContextTest.java
@@ -0,0 +1,84 @@
+package persistence.sql.entity.context;
+
+import domain.Person;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class PersistenceContextTest {
+
+
+    private PersistenceContext persistenceContext;
+    private Person person;
+
+    @BeforeEach
+    void setUp() {
+        this.person = new Person(1L, "박재성", 10, "jason");
+        this.persistenceContext = new PersistenceContextImpl();
+
+        persistenceContext.addEntity(person, person.getId());
+    }
+
+    @DisplayName("1차 캐시에 저장된 값 가져온다.")
+    @Test
+    void getEntityTest() {
+        Person entity = persistenceContext.getEntity(Person.class, 1L);
+
+        assertThat(entity).isEqualTo(person);
+    }
+
+    @DisplayName("1차 캐시에 저장된 값이 없을 경우 널을 반환한다.")
+    @Test
+    void isExistsEntityTest() {
+        Person entity = persistenceContext.getEntity(Person.class, 2L);
+
+        assertThat(entity).isNull();
+    }
+
+    @DisplayName("1차 캐시와 스냅샷에도 저장이 된다.")
+    @Test
+    void addEntityTest() {
+        Person person2 = new Person(2L, "김성주", 12, "jpa@nextstep.com");
+        persistenceContext.addEntity(person2, person2.getId());
+
+        Person cacheEntity = persistenceContext.getEntity(Person.class, 2L);
+        Person snapshotEntity = persistenceContext.getDatabaseSnapshot(Person.class, 2L);
+
+        assertAll(
+                () -> assertThat(cacheEntity).isEqualTo(person2),
+                () -> assertThat(snapshotEntity).isEqualTo(person2)
+        );
+    }
+
+    @DisplayName("1차캐시와 스냅샷 데이터를 삭제한다.")
+    @Test
+    void removeEntityTest() {
+        persistenceContext.removeEntity(person);
+
+        Person cacheEntity = persistenceContext.getEntity(Person.class, person.getId());
+        Person snapshotEntity = persistenceContext.getDatabaseSnapshot(Person.class, person.getId());
+
+        assertAll(
+                () -> assertThat(cacheEntity).isNull(),
+                () -> assertThat(snapshotEntity).isNull()
+        );
+    }
+
+    @DisplayName("캐시와 스냅샷 데이터를 삭제한다.")
+    @Test
+    void removeAllTest() {
+        persistenceContext.removeAll();
+
+        Person cacheEntity = persistenceContext.getEntity(Person.class, person.getId());
+        Person snapshotEntity = persistenceContext.getDatabaseSnapshot(Person.class, person.getId());
+
+        assertAll(
+                () -> assertThat(cacheEntity).isNull(),
+                () -> assertThat(snapshotEntity).isNull()
+        );
+    }
+
+}
