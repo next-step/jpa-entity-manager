a4fd3dfa32b0c487cfa5bf32077cfcd3fe5a6d3c
junseo <djawnstj44@naver.com>
2024-03-07 18:29:33 +0900

    refactor: Columns 클래스 수정
    Columns 에서 각 컬럼 이름과 값을 쿼리문으로 생성해주던 역할을 쿼리 빌더로 옮기는 작업을 했다.

--- src/main/java/persistence/sql/mapping/Columns.java
+++ src/main/java/persistence/sql/mapping/Columns.java
@@ -1,9 +1,6 @@
 package persistence.sql.mapping;
 
 import persistence.sql.dialect.Dialect;
-import persistence.sql.dml.ComparisonOperator;
-import persistence.sql.dml.LogicalOperator;
-import persistence.sql.dml.Where;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,54 +16,27 @@ public class Columns {
         this.pkColumns = columns.stream().filter(Column::isPk).collect(Collectors.toList());
     }
 
-    public String columnSetClause() {
-        return this.columns.stream()
-                .map(column -> column.getName() + " = " + column.getValue().getValueClause())
-                .collect(Collectors.joining(", "));
+    public List<Column> getColumns() {
+        return this.columns;
     }
 
-    public List<Where> generatePkColumnWheres() {
-        return this.pkColumns.stream()
-                .map(column -> new Where(column, column.getValue(), LogicalOperator.AND, new ComparisonOperator(ComparisonOperator.Comparisons.EQ)))
+    public List<Column> getPkColumns() {
+        return this.pkColumns;
+    }
+
+    public List<String> getColumnNames() {
+        return this.columns.stream()
+                .map(Column::getName)
                 .collect(Collectors.toList());
     }
 
-    public String columnNamesClause() {
-        return this.columns.stream()
-                .map(Column::getName)
-                .collect(Collectors.joining(", "));
-    }
-
-    public String pkColumnNamesClause(final Dialect dialect) {
+    public List<Column> getPkColumnsWithValueClause(final Dialect dialect) {
         return this.pkColumns.stream()
                 .filter(column -> isPkWithValueClause(column, dialect))
-                .map(Column::getName)
-                .collect(Collectors.joining(", "));
-    }
-
-    public String columnValuesClause() {
-        return this.columns.stream()
-                .map(Column::getValue)
-                .map(Value::getValueClause)
-                .collect(Collectors.joining(", "));
-    }
-
-    public String pkColumnValuesClause(final Dialect dialect) {
-        return this.pkColumns.stream()
-                .filter(column -> isPkWithValueClause(column, dialect))
-                .map(column -> getPkValueClause(column, dialect))
-                .collect(Collectors.joining(", "));
+                .collect(Collectors.toList());
     }
 
     private boolean isPkWithValueClause(final Column column, final Dialect dialect) {
         return !column.isIdentifierKey() || dialect.getIdentityColumnSupport().hasIdentityInsertKeyword();
-    }
-
-    private String getPkValueClause(final Column column, final Dialect dialect) {
-        if (column.isIdentifierKey() && dialect.getIdentityColumnSupport().hasIdentityInsertKeyword()) {
-            return dialect.getIdentityColumnSupport().getIdentityInsertString();
-        }
-
-        return column.getValue().getValueClause();
     }
 }
