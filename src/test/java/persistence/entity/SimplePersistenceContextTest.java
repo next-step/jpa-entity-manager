c8cb8ad9198eb6677300ef1a94504dd887bab3a0
ckyeon <qsky8520@gmail.com>
2024-03-08 15:01:21 +0900

    test(SimplePersistenceContext): 동일한 아이디를 가진 서로 다른 엔티티 클래스에 대한 케이스 추가

--- src/test/java/persistence/entity/SimplePersistenceContextTest.java
+++ src/test/java/persistence/entity/SimplePersistenceContextTest.java
@@ -3,7 +3,13 @@ package persistence.entity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import persistence.study.sql.ddl.Person2;
 import persistence.study.sql.ddl.Person3;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,29 +17,42 @@ class SimplePersistenceContextTest {
 
     private final PersistenceContext persistenceContext = new SimplePersistenceContext();
     private final Person3 person = new Person3(1L, "qwer1", 123, "email1@email.com");
+    private final Person2 person2 = new Person2(1L, "qwer2", 123, "email1@email.com");
 
     @BeforeEach
     void setUp() {
-        EntityId id = new EntityId(1L);
-        persistenceContext.addEntity(id, person);
+        EntityId personId = new EntityId(1L);
+        persistenceContext.addEntity(personId, person);
+
+        EntityId person2Id = new EntityId(1L);
+        persistenceContext.addEntity(person2Id, person2);
     }
 
     @DisplayName("1차 캐시에서 엔티티를 정상적으로 가져온다.")
-    @Test
-    void getEntity() {
-        EntityId id = new EntityId(1L);
-        Object result = persistenceContext.getEntity(Person3.class, id);
+    @ParameterizedTest
+    @MethodSource
+    void getEntity(Class<?> clazz, EntityId id, Object expect) {
+        Object result = persistenceContext.getEntity(clazz, id);
 
-        assertThat(result).isEqualTo(person);
+        assertThat(result).isEqualTo(expect);
+    }
+
+    private static Stream<Arguments> getEntity() {
+        return Stream.of(
+          Arguments.of(Person3.class, new EntityId(1L), new Person3(1L, "qwer1", 123, "email1@email.com")),
+          Arguments.of(Person2.class, new EntityId(1L), new Person2(1L, "qwer2", 123, "email1@email.com"))
+        );
     }
 
     @DisplayName("엔티티를 persistence context의 1차 캐시에 추가할 수 있다.")
     @Test
     void addEntity() {
         Person3 person = new Person3(2L, "qwer", 123, "email@email.com");
+        Person2 person2 = new Person2(2L, "qwe", 1234, "wqe@email.com");
 
         EntityId id = new EntityId(2L);
         persistenceContext.addEntity(id, person);
+        persistenceContext.addEntity(id, person2);
 
         Object result = persistenceContext.getEntity(Person3.class, id);
         assertThat(result).isEqualTo(person);
@@ -53,9 +72,12 @@ class SimplePersistenceContextTest {
     @Test
     void identity() {
         Person3 person = new Person3(2L, "qwer1", 123, "email1@email.com");
+        Person2 person2 = new Person2(2L, "qwer1", 123, "email1@email.com");
 
         EntityId id = new EntityId(2L);
         persistenceContext.addEntity(id, person);
+        persistenceContext.addEntity(id, person2);
+
         Object result = persistenceContext.getEntity(Person3.class, id);
 
         assertThat(person == result).isTrue();
