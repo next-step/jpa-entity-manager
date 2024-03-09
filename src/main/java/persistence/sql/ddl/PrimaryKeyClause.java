edcf66d4caf273e394796168f4a557128adab402
daheecha0612 <dahee.cha@gmail.com>
2024-03-09 17:17:53 +0900

    (refactor) 변수명명 없이 바로 리턴하라

--- src/main/java/persistence/sql/ddl/PrimaryKeyClause.java
+++ src/main/java/persistence/sql/ddl/PrimaryKeyClause.java
@@ -63,14 +63,11 @@ public class PrimaryKeyClause {
 
 
         idField.setAccessible(true);
-        Long id;
         try {
-            id = (Long) idField.get(entity);
+            return (Long) idField.get(entity);
         } catch (IllegalAccessException e) {
             throw new InvalidPrimaryKeyException();
         }
-
-        return id;
     }
 
     public String name() {
