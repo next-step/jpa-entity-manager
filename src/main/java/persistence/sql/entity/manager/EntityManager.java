f31f3cc5c3be69d570b6e4b7453a5a53dcdde0a9
Shin seongsu <tlstjdtn321@naver.com>
2024-03-09 22:37:15 +0900

    feat: readOnlyFind 정의

--- src/main/java/persistence/sql/entity/manager/EntityManager.java
+++ src/main/java/persistence/sql/entity/manager/EntityManager.java
@@ -8,6 +8,8 @@ public interface EntityManager {
 
     <T> T find(Class<T> clazz, Object id);
 
+    <T> T findOfReadOnly(Class<T> clazz, Object id);
+
     void persist(Object entity);
 
     void remove(Object entity);
