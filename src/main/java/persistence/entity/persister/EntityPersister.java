d3c8bb219997b7d32dd6a1d655bbeaf48cf6c22b
junseo <djawnstj44@naver.com>
2024-03-11 08:16:32 +0900

    feat: EntityPErsister 기능 개발
    엔터티 별 식별키로 사용할 객체를 반환받는 기능을 위해 getIdentifier() 메서드를 추가했다.

--- src/main/java/persistence/entity/persister/EntityPersister.java
+++ src/main/java/persistence/entity/persister/EntityPersister.java
@@ -1,11 +1,13 @@
-package persistence.entity;
+package persistence.entity.persister;
 
 public interface EntityPersister {
 
     boolean update(final Object entity);
 
-    void insert(final Object entity);
+    Object insert(final Object entity);
 
     void delete(final Object entity);
+
+    Object getIdentifier(Object entity);
 
 }
