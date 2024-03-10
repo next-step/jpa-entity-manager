ca99bf1bb0323495a549a077d1932c38fcd8b14d
junseo <djawnstj44@naver.com>
2024-03-11 08:36:53 +0900

    feat: EntityManager 1차 캐시 적용

--- src/main/java/persistence/entity/SimpleEntityManager.java
+++ src/main/java/persistence/entity/SimpleEntityManager.java
@@ -1,28 +1,56 @@
 package persistence.entity;
 
+import persistence.entity.context.PersistenceContext;
+import persistence.entity.context.StatefulPersistenceContext;
+import persistence.entity.loader.EntityLoader;
+import persistence.entity.persister.EntityPersister;
+import persistence.model.MappingMetaModel;
+
 public class SimpleEntityManager implements EntityManager {
 
-    private final EntityPersister entityPersister;
+    private final MappingMetaModel mappingMetaModel;
     private final EntityLoader entityLoader;
+    private final PersistenceContext persistenceContext;
 
-    public SimpleEntityManager(final EntityPersister entityPersister, final EntityLoader entityLoader) {
-        this.entityPersister = entityPersister;
+    public SimpleEntityManager(final MappingMetaModel mappingMetaModel, final EntityLoader entityLoader) {
+        this.mappingMetaModel = mappingMetaModel;
         this.entityLoader = entityLoader;
+        this.persistenceContext = createPersistenceContext();
+    }
+
+    protected PersistenceContext createPersistenceContext() {
+        return new StatefulPersistenceContext();
     }
 
     @Override
     public <T> T find(final Class<T> clazz, final Object key) {
+        final T cachedEntity = persistenceContext.getEntity(key, clazz.getName());
+        if (cachedEntity != null) {
+            return cachedEntity;
+        }
+
         return entityLoader.load(clazz, key);
     }
 
     @Override
     public void persist(final Object entity) {
-        entityPersister.insert(entity);
+        final String entityName = entity.getClass().getName();
+        final EntityPersister persister = mappingMetaModel.getEntityDescriptor(entityName);
+
+        final Object key = persister.insert(entity);
+
+        persistenceContext.addEntity(key, entity);
     }
 
     @Override
     public void remove(final Object entity) {
-        entityPersister.delete(entity);
+        final String entityName = entity.getClass().getName();
+        final EntityPersister persister = mappingMetaModel.getEntityDescriptor(entityName);
+
+        final Object identifier = persister.getIdentifier(entity);
+        persistenceContext.removeEntity(identifier, entity);
+
+        persister.delete(entity);
     }
 
 }
