9abed72cad31034d970695989d1f9ae24b4dbe2b
Shinyoung Kim <rolroralra@gmail.com>
2024-03-12 23:06:48 +0900

    feat: 0단계 - 기본 코드 준비 (#168)
    Co-authored-by: socar-shinyoung <shinyoung@socar.kr>

---
+++ src/main/java/persistence/sql/ddl/common/StringConstants.java
@@ -0,0 +1,19 @@
+package persistence.sql.ddl.common;
+
+public class StringConstants {
+
+    private StringConstants() {
+        // Do Nothing
+    }
+
+    public static final String EMPTY_STRING = "";
+
+    public static final CharSequence SPACE = " ";
+
+    public static final String SCHEMA_TABLE_DELIMITER = ".";
+
+    public static final String COLUMN_DEFINITION_DELIMITER = ", ";
+
+    public static final String PRIMARY_KEY_NOT_FOUND = "Primary key not found";
+
+}
