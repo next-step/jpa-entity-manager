dd14364dd61cf4faf585e5d951f2d5c899398fa5
JUNSEO <djawnstj44@naver.com>
2024-03-12 17:13:29 +0900

    test: PersistenceContextTest checkDirty 테스트 추가

--- src/test/java/persistence/entity/context/PersistenceContextTest.java
+++ src/test/java/persistence/entity/context/PersistenceContextTest.java
@@ -2,12 +2,14 @@ package persistence.entity.context;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import persistence.EntityMetaDataTestSupport;
 import persistence.PersonV3FixtureFactory;
 import persistence.sql.ddl.PersonV3;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-class PersistenceContextTest {
+class PersistenceContextTest extends EntityMetaDataTestSupport {
 
     private final PersistenceContext persistenceContext = new StatefulPersistenceContext();
 
@@ -22,7 +24,7 @@ class PersistenceContextTest {
         final Object cachedEntity = persistenceContext.getEntity(person.getId(), person.getClass().getName());
 
         // then
-        assertEquals(person, cachedEntity);
+        assertThat(cachedEntity).isEqualTo(person);
     }
 
     @DisplayName("엔티티를 1차 캐시에 저장한다")
@@ -36,7 +38,7 @@ class PersistenceContextTest {
 
         // then
         final Object cachedEntity = persistenceContext.getEntity(person.getId(), person.getClass().getName());
-        assertEquals(person, cachedEntity);
+        assertThat(cachedEntity).isEqualTo(person);
     }
 
     @DisplayName("엔티티를 1차 캐시에서 삭제한다")
@@ -51,7 +53,29 @@ class PersistenceContextTest {
 
         // then
         final Object cachedEntity = persistenceContext.getEntity(person.getId(), person.getClass().getName());
-        assertNull(cachedEntity);
+        assertThat(cachedEntity).isNull();
+    }
+
+    @DisplayName("엔티티의 변경점이 존재하는지 확인한다")
+    @Test
+    public void checkDirty() throws Exception {
+        // given
+        final PersonV3 person1 = PersonV3FixtureFactory.generatePersonV3Stub(1L);
+        final PersonV3 person2 = PersonV3FixtureFactory.generatePersonV3Stub(2L);
+        persistenceContext.addEntity(person1.getId(), person1);
+        persistenceContext.addEntity(person2.getId(), person2);
+
+        person2.setName("update name");
+
+        // when
+        final boolean isDirty1 = persistenceContext.checkDirty(person1.getId(), person1);
+        final boolean isDirty2 = persistenceContext.checkDirty(person2.getId(), person2);
+
+        // then
+        assertAll(
+                () -> assertThat(isDirty1).isFalse(),
+                () -> assertThat(isDirty2).isTrue()
+        );
     }
 
 }
