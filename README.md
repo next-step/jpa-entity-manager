6de4776cc5f0532d24343086cde6f34d86135ff8
joel-you <ruew12@gmail.com>
2024-03-13 02:05:08 +0900

    feat(EntityLoader.java): 요구사항 구현
    - EntityLoader 구현
    - EntityManager 에서의 책임을 EntityLoader로 이관
    -
    EntityManager 의 불필요한 필드 제거

--- README.md
+++ README.md
@@ -15,11 +15,11 @@
 
 #### 요구사항
 
-* [ ] 요구사항 1 - RowMapper 리팩터링
+* [x] 요구사항 1 - RowMapper 리팩터링
     * 아래와 같이 새로운 엔티티가 생길 때마다 매번 RowMapper를 만들어야 할까? 동적으로 생성해 보자
     * > EntityLoader라는 클래스를 만들어 구현<br>
       ResultSet의 metaData를 잘활용해보자<br>
       EntityLoader는 데이터베이스 쿼리를 실행하여 엔티티 객체를 로드하는 역할을 수행합니다.
 
-* [ ] 요구사항 2 - EntityManager 의 책임 줄여주기
+* [x] 요구사항 2 - EntityManager 의 책임 줄여주기
     * EntityManager 의 구현체에서 find 에 대한 책임을 EntityLoader 로 옮겨주자
---
+++ src/main/java/persistence/entity/EntityLoader.java
@@ -0,0 +1,45 @@
+package persistence.entity;
+
+import jdbc.EntityRowMapper;
+import jdbc.JdbcTemplate;
+import persistence.sql.dialect.Dialect;
+import persistence.sql.dml.SelectQueryBuilder;
+import persistence.sql.dml.conditions.WhereRecord;
+import persistence.sql.metadata.EntityMetadata;
+import persistence.sql.metadata.PrimaryKeyMetadata;
+
+import java.util.List;
+
+public class EntityLoader {
+    private final JdbcTemplate jdbcTemplate;
+    private final Dialect dialect;
+
+    public EntityLoader(JdbcTemplate jdbcTemplate, Dialect dialect) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.dialect = dialect;
+    }
+
+    public <T> T find(Class<T> clazz, Object Id) {
+        SelectQueryBuilder selectQueryBuilder = SelectQueryBuilder.builder()
+                .dialect(dialect)
+                .entity(clazz)
+                .where(List.of(WhereRecord.of("id", "=", Id)))
+                .build();
+
+        return jdbcTemplate.queryForObject(selectQueryBuilder.generateQuery(), resultSet -> new EntityRowMapper<>(clazz).mapRow(resultSet));
+    }
+
+    public <T> T findByEntity(T entity) {
+        Class<T> clazz = (Class<T>) entity.getClass();
+        EntityMetadata metadata = EntityMetadata.of(clazz, entity);
+        PrimaryKeyMetadata primaryKey = metadata.getPrimaryKey();
+
+        SelectQueryBuilder selectQueryBuilder = SelectQueryBuilder.builder()
+                .dialect(dialect)
+                .entity(clazz)
+                .where(List.of(WhereRecord.of(primaryKey.getName(), "=", primaryKey.getValue())))
+                .build();
+
+        return jdbcTemplate.queryForObject(selectQueryBuilder.generateQuery(), resultSet -> new EntityRowMapper<>(clazz).mapRow(resultSet));
+    }
+}
