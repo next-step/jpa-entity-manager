0f5d8d125acf4ba611bb15c78b92d4e60cc401a7
parkje0927 <parkje09270@gmail.com>
2024-03-13 23:48:35 +0900

    feat: step4 - EntityManager 내 EntityEntry 의존성 주입 추가

--- src/main/java/persistence/entity/SimpleEntityManager.java
+++ src/main/java/persistence/entity/SimpleEntityManager.java
@@ -1,6 +1,5 @@
 package persistence.entity;
 
-import dialect.Dialect;
 import persistence.context.PersistenceContext;
 
 import java.util.Objects;
@@ -13,46 +12,58 @@ public class SimpleEntityManager implements EntityManager {
      * 3. 트랜잭션 커밋 후 해당 스냅샷과 현재 Entity 를 비교 (데이터베이스 커밋은 신경쓰지 않는다)
      * 4. 다른 점을 쿼리로 생성
      */
-    private final Dialect dialect;
     private final EntityPersister entityPersister;
     private final EntityLoader entityLoader;
     private final PersistenceContext persistenceContext;
+    private final EntityEntry entityEntry;
 
-    public SimpleEntityManager(Dialect dialect, EntityPersister entityPersister, EntityLoader entityLoader,
-                               PersistenceContext persistenceContext) {
-        this.dialect = dialect;
+    public SimpleEntityManager(EntityPersister entityPersister, EntityLoader entityLoader,
+                               PersistenceContext persistenceContext, EntityEntry entityEntry) {
         this.entityPersister = entityPersister;
         this.entityLoader = entityLoader;
         this.persistenceContext = persistenceContext;
+        this.entityEntry = entityEntry;
     }
 
     @Override
     public <T> T find(Object entity, Class<T> clazz, Long id) {
+        entityEntry.preFind();
+
         T cachedEntity = persistenceContext.getEntity(clazz, id);
         if (Objects.nonNull(cachedEntity)) {
+            entityEntry.postFind();
             return cachedEntity;
         }
         T loadedEntity = entityLoader.findById(clazz, entity, id);
         persistenceContext.addEntity(id, loadedEntity);
+
+        entityEntry.postFind();
         return loadedEntity;
     }
 
     @Override
     public <T> T persist(T entity) {
+        entityEntry.prePersist();
         entityPersister.insert(entity);
         persistenceContext.addEntity(entity);
+        entityEntry.postPersist();
         return entity;
     }
 
     @Override
     public boolean update(Object entity) {
-        return entityPersister.update(entity);
+        entityEntry.preUpdate();
+        boolean update = entityPersister.update(entity);
+        entityEntry.postUpdate();
+        return update;
     }
 
     @Override
     public void remove(Object entity) {
+        entityEntry.preRemove();
         entityPersister.delete(entity);
         persistenceContext.removeEntity(entity);
+        entityEntry.postRemove();
     }
 
     /**
@@ -65,8 +76,10 @@ public class SimpleEntityManager implements EntityManager {
         EntitySnapshot entitySnapshot = new EntitySnapshot(entity);
 
         if (!Objects.equals(cachedDatabaseSnapshot, entitySnapshot)) {
+            entityEntry.preUpdate();
             update(entity);
             persistenceContext.addEntity(entity);
+            entityEntry.postUpdate();
         }
         return entity;
     }
