827a8460ea66041ec13dd855dea04e7f04c3c771
joel-you <ruew12@gmail.com>
2024-03-18 01:22:34 +0900

    refactor(EntityKey): EntityId to EntityKey로 변경

--- src/main/java/persistence/entity/EntityKey.java
+++ src/main/java/persistence/entity/EntityKey.java
@@ -2,17 +2,17 @@ package persistence.entity;
 
 import persistence.sql.metadata.EntityMetadata;
 
-public class EntityId {
+public class EntityKey {
     private final EntityMetadata entityMetadata;
     private final Object id;
 
-    private EntityId(Class<?> clazz, Object id) {
+    private EntityKey(Class<?> clazz, Object id) {
         this.entityMetadata = EntityMetadata.from(clazz);
         this.id = id;
     }
 
-    public static EntityId of(Class<?> clazz, Object id) {
-        return new EntityId(clazz, id);
+    public static EntityKey of(Class<?> clazz, Object id) {
+        return new EntityKey(clazz, id);
     }
 
     public Class<?> getClazz() {
