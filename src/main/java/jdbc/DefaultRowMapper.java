c113877bd3d66f382429c0bcbf0a12f275a4ec8a
Jeongsu Yang <mycat83@gmail.com>
2024-10-20 01:48:10 +0900

    fix: DefaultRowMapper 버그 픽스

--- src/main/java/jdbc/DefaultRowMapper.java
+++ src/main/java/jdbc/DefaultRowMapper.java
@@ -1,11 +1,15 @@
 package jdbc;
 
+import jakarta.persistence.Transient;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
 
 public class DefaultRowMapper<T> implements RowMapper<T> {
     private static final Logger logger = LoggerFactory.getLogger(DefaultRowMapper.class);
@@ -19,12 +23,19 @@ public class DefaultRowMapper<T> implements RowMapper<T> {
     @Override
     public T mapRow(ResultSet resultSet) {
         final T entity = new InstanceFactory<>(clazz).createInstance();
-        final Field[] fields = clazz.getDeclaredFields();
+        final List<Field> fields = getPersistentFields();
 
-        for (int i = 0; i < fields.length; i++) {
-            mapField(resultSet, entity, fields[i], i + 1);
+        for (int i = 0; i < fields.size(); i++) {
+            mapField(resultSet, entity, fields.get(i), i + 1);
         }
         return entity;
+    }
+
+    @NotNull
+    private List<Field> getPersistentFields() {
+        return Arrays.stream(clazz.getDeclaredFields())
+                .filter(field -> !field.isAnnotationPresent(Transient.class))
+                .toList();
     }
 
     private void mapField(ResultSet resultSet, T entity, Field field, int columnIndex) {
