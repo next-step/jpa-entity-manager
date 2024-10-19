5c87de1cbd7679cd44cad217c465cc9ecbcba8cc
chanho0912-kim <chanho0912.kim@linecorp.com>
2024-10-19 16:27:12 +0900

    test: PersistenceContextTest 추가

--- src/test/java/persistence/entity/PersistenceContextTest.java
+++ src/test/java/persistence/entity/PersistenceContextTest.java
@@ -1,5 +1,61 @@
 package persistence.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class PersistenceContextTest {
+
+    @Entity(name = "TestEntity")
+    private static class TestEntity {
+        @Id
+        private Long id;
+        private String name;
+
+        protected TestEntity() {
+        }
+
+        public TestEntity(Long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
+    @Test
+    void addEntity() {
+        PersistenceContext persistenceContext = new PersistenceContextImpl();
+        EntityKey entityKey = new EntityKey(1L, "Entity");
+        Object entity = new TestEntity(1L, "Test");
+
+        persistenceContext.addEntity(entityKey, entity);
+
+        assertThat(persistenceContext.findEntity(entityKey)).isEqualTo(entity);
+    }
+
+    @Test
+    void shouldDoNothingWhenAddTwice() {
+        PersistenceContext persistenceContext = new PersistenceContextImpl();
+        EntityKey entityKey = new EntityKey(1L, "Entity");
+        Object entity = new TestEntity(1L, "Test");
+
+        persistenceContext.addEntity(entityKey, entity);
+        persistenceContext.addEntity(entityKey, entity);
+
+        assertThat(persistenceContext.findEntity(entityKey)).isEqualTo(entity);
+    }
+
+    @Test
+    void removeEntity() {
+        PersistenceContext persistenceContext = new PersistenceContextImpl();
+        EntityKey entityKey = new EntityKey(1L, "Entity");
+        Object entity = new TestEntity(1L, "Test");
+
+        persistenceContext.addEntity(entityKey, entity);
+        persistenceContext.removeEntity(entityKey);
+
+        assertThat(persistenceContext.findEntity(entityKey)).isNull();
+    }
 
 }
