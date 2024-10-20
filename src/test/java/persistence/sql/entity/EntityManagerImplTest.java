979b7c1e8c6303efd4ba58e0342ac1eeb85dac03
yangseung-in <rhfpdk92@naver.com>
2024-10-20 19:44:24 +0900

    test: idValue가 null인경우 테스트 추가

--- src/test/java/persistence/sql/entity/EntityManagerImplTest.java
+++ src/test/java/persistence/sql/entity/EntityManagerImplTest.java
@@ -34,7 +34,7 @@ class EntityManagerImplTest {
         Metadata metadata = new Metadata(Person.class);
         CreateQueryBuilder queryBuilder = new CreateQueryBuilder(Person.class);
         String tableQuery = queryBuilder.createTableQuery(Person.class);
-        InsertQueryBuilder insertQueryBuilder = new InsertQueryBuilder(metadata.getEntityTable(),metadata.getEntityColumns());
+        InsertQueryBuilder insertQueryBuilder = new InsertQueryBuilder(metadata.getEntityTable(), metadata.getEntityColumns());
         Person person = new Person("yang", 23, "rhfp@naver.com", 3);
         String insertQuery = insertQueryBuilder.getInsertQuery(person);
 
@@ -91,6 +91,20 @@ class EntityManagerImplTest {
 
 
     @Test
+    @DisplayName("EntityManager의 persist시 idValue가 null인 경우")
+    void entityManager_persist_with_null_id() {
+        long expectedId = 2L;
+        Person expectPerson = new Person(null, "yang2", 25, "rhfpdk92@naver.com");
+
+        entityManager.persist(expectPerson);
+
+        Person resultPerson = entityManager.find(Person.class, expectedId);
+
+        assertThat(resultPerson.getId()).isEqualTo(expectedId);
+    }
+
+
+    @Test
     @DisplayName("EntityManager의 remove구현")
     void entityManager_remove() {
         Person expectPerson = new Person(2L, "yang2", 25, "rhfpdk92@naver.com");
@@ -101,7 +115,7 @@ class EntityManagerImplTest {
         assertThrows(RuntimeException.class, () -> entityManager.find(Person.class, 2L));
     }
 
-//    @Test
+    //    @Test
     @DisplayName("EntityManager의 update구현")
     void entityManager_update() {
         Person person = new Person(2L, "yang2", 25, "rhfpdk92@naver.com");
