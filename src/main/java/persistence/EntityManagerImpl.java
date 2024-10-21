577c80815eee9243f504200146b18ceedc79cdcb
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-21 14:46:49 +0900

    feat: EntityPersister 생성

--- src/main/java/persistence/EntityManagerImpl.java
+++ src/main/java/persistence/EntityManagerImpl.java
@@ -1,63 +1,37 @@
 package persistence;
 
-import builder.dml.DMLBuilderData;
-import builder.dml.builder.*;
-import jdbc.EntityMapper;
-import jdbc.JdbcTemplate;
-
 import java.util.List;
 
 public class EntityManagerImpl implements EntityManager {
 
-    private final static String DATA_NOT_EXIST_MESSAGE = "데이터가 존재하지 않습니다. : ";
+    private final EntityPersister entityPersister;
 
-    private final JdbcTemplate jdbcTemplate;
-
-    public EntityManagerImpl(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public EntityManagerImpl(EntityPersister entityPersister) {
+        this.entityPersister = entityPersister;
     }
 
     @Override
     public <T> T find(Class<T> clazz, Long id) {
-        SelectByIdQueryBuilder queryBuilder = new SelectByIdQueryBuilder();
-        return jdbcTemplate.queryForObject(queryBuilder.buildQuery(DMLBuilderData.createDMLBuilderData(clazz, id)), resultSet -> EntityMapper.mapRow(resultSet, clazz));
+        return this.entityPersister.find(clazz, id);
     }
 
     @Override
     public <T> List<T> findAll(Class<T> clazz) {
-        SelectAllQueryBuilder queryBuilder = new SelectAllQueryBuilder();
-        return jdbcTemplate.query(queryBuilder.buildQuery(DMLBuilderData.createDMLBuilderData(clazz)), resultSet -> EntityMapper.mapRow(resultSet, clazz));
+        return this.entityPersister.findAll(clazz);
     }
 
     @Override
     public void persist(Object entityInstance) {
-        InsertQueryBuilder queryBuilder = new InsertQueryBuilder();
-        jdbcTemplate.execute(queryBuilder.buildQuery(DMLBuilderData.createDMLBuilderData(entityInstance)));
+        this.entityPersister.persist(entityInstance);
     }
 
     @Override
     public void update(Object entityInstance) {
-        confirmEntityDataExist(entityInstance);
-        UpdateQueryBuilder queryBuilder = new UpdateQueryBuilder();
-        jdbcTemplate.execute(queryBuilder.buildQuery(DMLBuilderData.createDMLBuilderData(entityInstance)));
+        this.entityPersister.update(entityInstance);
     }
 
     @Override
     public void remove(Object entityInstance) {
-        DeleteQueryBuilder queryBuilder = new DeleteQueryBuilder();
-        jdbcTemplate.execute(queryBuilder.buildQuery(DMLBuilderData.createDMLBuilderData(entityInstance)));
-    }
-
-    //조회되는 데이터가 존재하는지 확인한다.
-    private void confirmEntityDataExist(Object entityInstance) {
-        SelectByIdQueryBuilder queryBuilder = new SelectByIdQueryBuilder();
-        try {
-            jdbcTemplate.queryForObject(
-                    queryBuilder.buildQuery(DMLBuilderData.createDMLBuilderData(entityInstance)),
-                    resultSet -> EntityMapper.mapRow(resultSet, entityInstance.getClass())
-            );
-        } catch (RuntimeException e) {
-            throw new RuntimeException(DATA_NOT_EXIST_MESSAGE + entityInstance.getClass().getSimpleName());
-        }
+        this.entityPersister.remove(entityInstance);
     }
 }
