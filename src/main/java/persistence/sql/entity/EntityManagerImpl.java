39f274a4378ebccfd321eb4498cbad2855604239
yangseung-in <rhfpdk92@naver.com>
2024-10-22 23:57:09 +0900

    fix: 1차캐시와 상관없이 동작하도록 변경

--- src/main/java/persistence/sql/entity/EntityManagerImpl.java
+++ src/main/java/persistence/sql/entity/EntityManagerImpl.java
@@ -18,12 +18,7 @@ public class EntityManagerImpl implements EntityManager {
         if (persistenceContext.containsEntity(clazz, id)) {
             return persistenceContext.getEntity(clazz, id);
         }
-
-        T entity = entityPersister.select(clazz, id);
-        if (entity != null) {
-            persistenceContext.addEntity(entity, id);
-        }
-        return entity;
+        return entityLoader.loadEntity(clazz, id);
     }
 
     @Override
@@ -33,7 +28,9 @@ public class EntityManagerImpl implements EntityManager {
             entityPersister.insert(entity);
             idValue = entityPersister.getIdValue(entity);
         }
-        entityPersister.update(entity);
+        if (idValue != null) {
+            entityPersister.update(entity);
+        }
         persistenceContext.addEntity(entity, idValue);
         return entity;
     }
@@ -41,19 +38,20 @@ public class EntityManagerImpl implements EntityManager {
     @Override
     public void remove(Object entity) {
         Long idValue = entityPersister.getIdValue(entity);
-        if (idValue != null && persistenceContext.containsEntity(entity.getClass(), idValue)) {
-            entityPersister.delete(entity);
-            persistenceContext.removeEntity(entity.getClass(), idValue);
+        if (idValue == null) {
+            return;
         }
+
+        entityPersister.delete(entity);
+        persistenceContext.removeEntity(entity.getClass(), idValue);
+
     }
 
     @Override
     public Object update(Object entity) {
         Long idValue = entityPersister.getIdValue(entity);
-        if (persistenceContext.containsEntity(entity.getClass(), idValue)) {
-            entityPersister.update(entity);
-            persistenceContext.addEntity(entity.getClass(), idValue);
-        }
+        entityPersister.update(entity);
+        persistenceContext.addEntity(entity.getClass(), idValue);
         return entity;
     }
 
