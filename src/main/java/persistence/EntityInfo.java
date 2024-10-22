14de31add8a70f3da3c98282d86f9c7cc45bec90
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-23 00:38:13 +0900

    refactor: 생성자로 객체 생성하도록 수정

--- src/main/java/persistence/EntityInfo.java
+++ src/main/java/persistence/EntityInfo.java
@@ -7,13 +7,9 @@ public class EntityInfo<T> {
     private final Object id;
     private final Class<T> clazz;
 
-    private EntityInfo(Object id, Class<T> clazz) {
+    public EntityInfo(Object id, Class<T> clazz) {
         this.id = id;
         this.clazz = clazz;
-    }
-
-    public static <T> EntityInfo<T> createEntityInfo(Object id, Class<T> clazz) {
-        return new EntityInfo<>(id, clazz);
     }
 
     public Object getId() {
@@ -22,14 +18,13 @@ public class EntityInfo<T> {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) return true; // 같은 객체인지 확인
-        if (!(obj instanceof EntityInfo)) return false; // 타입 확인
-        EntityInfo<?> other = (EntityInfo<?>) obj; // 형변환
-        return Objects.equals(id, other.id) && clazz.equals(other.clazz); // id와 clazz 비교
+        if (this == obj) return true;
+        if (!(obj instanceof EntityInfo<?> other)) return false;
+        return Objects.equals(id, other.id) && clazz.equals(other.clazz);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, clazz); // id와 clazz의 해시 코드 반환
+        return Objects.hash(id, clazz);
     }
 }
