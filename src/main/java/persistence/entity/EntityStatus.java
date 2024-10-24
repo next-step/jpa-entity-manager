631f4f7f5106a8afb5fd644ed526d3f0febfe82d
Jeongsu Yang <mycat83@gmail.com>
2024-10-25 00:31:10 +0900

    fix: 오타 수정

--- src/main/java/persistence/entity/EntityStatus.java
+++ src/main/java/persistence/entity/EntityStatus.java
@@ -4,7 +4,7 @@ public enum EntityStatus {
     MANAGED,    // 영속 상태
     READ_ONLY,  // 읽기 전용 상태
     DELETED,    // 삭제 예정 상태
-    GONE,       // 역속석 컨텍스트에서 제거된 상태
+    GONE,       // 영속성 컨텍스트에서 제거된 상태
     LOADING,    // 로딩 중 상태
     SAVING      // 저장 중 상태
 }
