2c6e9b72de0d87bd87ace7dc40e674a26ec13a15
hellonayeon <hellonykwon@gmail.com>
2024-10-27 19:29:36 +0900

    test: UpdateQueryBuilder 성공 케이스

--- src/test/java/persistence/sql/dml/query/builder/UpdateQueryBuilderTest.java
+++ src/test/java/persistence/sql/dml/query/builder/UpdateQueryBuilderTest.java
@@ -1,4 +1,29 @@
+package persistence.sql.dml.query.builder;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import persistence.sql.dialect.H2Dialect;
+import persistence.sql.dml.query.UpdateQuery;
+import persistence.sql.dml.query.WhereCondition;
+import sample.domain.Person;
+
 class UpdateQueryBuilderTest {
-  
+
+    @Test
+    @DisplayName("[성공] Person Entity 테이블의 특정 id에 대한 update query")
+    void updateQuery() {
+        Person person = new Person("person name", 20, "person@email.com");
+        UpdateQuery query = new UpdateQuery(person);
+        UpdateQueryBuilder queryBuilder = UpdateQueryBuilder.builder(new H2Dialect())
+                .update(query.tableName())
+                .set(query.columns())
+                .where(List.of(new WhereCondition("id", "=", 1L)));
+        String expectedQuery = """
+                update users set nick_name = 'person name', old = 20, email = 'person@email.com' where id = 1""";
+        assertEquals(queryBuilder.build(), expectedQuery);
+    }
+
 }
