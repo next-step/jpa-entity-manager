0f1a14cb8b8d9790fd7fb7e74098679b0a81587e
yangseung-in <rhfpdk92@naver.com>
2024-10-27 22:59:50 +0900

    chore: EntityStatus, EntityEntry 추가

---
+++ src/main/java/persistence/sql/entity/EntityEntry.java
@@ -0,0 +1,17 @@
+package persistence.sql.entity;
+
+import java.io.Serializable;
+
+public class EntityEntry {
+    private EntityStatus entityStatus;
+    private Serializable id;
+
+    public EntityEntry(EntityStatus entityStatus, Serializable id) {
+        this.entityStatus = entityStatus;
+        this.id = id;
+    }
+
+    public void updateStatus(EntityStatus entityStatus) {
+        this.entityStatus = entityStatus;
+    }
+}
