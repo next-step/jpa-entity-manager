3d021b2cddaf2f19ff0bfad779acde1767d1a367
dohoonkim <dohoonkim023@gmail.com>
2024-10-27 10:05:06 +0900

    refactor: EntityPersisterStore 생성하여 객체 캐싱

---
+++ src/main/java/persistence/entity/EntityPersisterStore.java
@@ -0,0 +1,28 @@
+package persistence.entity;
+
+import java.util.HashMap;
+import java.util.Map;
+import jdbc.JdbcTemplate;
+
+public class EntityPersisterStore {
+
+    Map<Class<?>, EntityPersister> entityPersisters;
+    JdbcTemplate jdbcTemplate;
+
+    public EntityPersisterStore(JdbcTemplate jdbcTemplate) {
+        this.entityPersisters = new HashMap<>();
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public EntityPersister getEntityPersister(Class<?> entityClass) {
+        if (!entityPersisters.containsKey(entityClass)) {
+            entityPersisters.put(entityClass, new EntityPersister(entityClass, jdbcTemplate));
+        }
+        return entityPersisters.get(entityClass);
+    }
+
+    private void addEntityPersister(Class<?> entityClass, EntityPersister entityPersister) {
+        entityPersisters.put(entityClass, entityPersister);
+    }
+
+}
--- src/main/java/persistence/entity/impl/EntityManagerImpl.java
+++ src/main/java/persistence/entity/impl/EntityManagerImpl.java
@@ -3,17 +3,19 @@ package persistence.entity.impl;
 import java.sql.Connection;
 import jdbc.TransactionalJdbcTemplate;
 import persistence.entity.EntityManager;
-import persistence.entity.EntityPersister;
+import persistence.entity.EntityPersisterStore;
 import persistence.entity.PersistenceContext;
 
 public class EntityManagerImpl implements EntityManager {
 
     private final PersistenceContext persistenceContext;
     private final TransactionalJdbcTemplate transactionalJdbcTemplate;
+    private final EntityPersisterStore entityPersisterStore;
 
     public EntityManagerImpl(Connection connection) {
         this.transactionalJdbcTemplate = new TransactionalJdbcTemplate(connection);
         this.persistenceContext = new PersistenceContextImpl(transactionalJdbcTemplate);
+        this.entityPersisterStore = new EntityPersisterStore(transactionalJdbcTemplate);
     }
 
     @Override
@@ -39,10 +41,10 @@ public class EntityManagerImpl implements EntityManager {
     @Override
     public void flush() throws IllegalAccessException {
         for (Object entity : persistenceContext.getPendingEntities()) {
-            new EntityPersister<>(entity.getClass(), transactionalJdbcTemplate).insert(entity);
+            entityPersisterStore.getEntityPersister(entity.getClass()).insert(entity);
         }
         for (Object entity : persistenceContext.getPersistedEntities()) {
-            new EntityPersister<>(entity.getClass(), transactionalJdbcTemplate).update(entity);
+            entityPersisterStore.getEntityPersister(entity.getClass()).update(entity);
         }
     }
 
