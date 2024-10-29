9ce8789b56b778aab6c96bc1f267b73ba8b64d36
lhs0610 <lhs0610@midasin.com>
2024-10-29 12:15:47 +0900

    test: EntityEntry 테스트 케이스 추가

--- src/test/java/persistence/sql/entity/EntityEntryTest.java
+++ src/test/java/persistence/sql/entity/EntityEntryTest.java
@@ -2,10 +2,21 @@ package persistence.sql.entity;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import persistence.sql.context.KeyHolder;
+import persistence.sql.dml.MetadataLoader;
 import persistence.sql.dml.TestEntityInitialize;
+import persistence.sql.dml.impl.SimpleMetadataLoader;
 import persistence.sql.entity.data.Status;
 import persistence.sql.fixture.TestPerson;
 import persistence.sql.fixture.TestPersonNoGenerateValue;
+import persistence.util.ReflectionUtils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -39,5 +50,138 @@ class EntityEntryTest extends TestEntityInitialize {
         assertThatThrownBy(() -> EntityEntry.newEntry(person, Status.MANAGED))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage("Primary key must not be null");
+    }
+
+    @Test
+    @DisplayName("synchrnoizingSnapshot 함수는 Entity의 스냅샷을 업데이트한다.")
+    void testSynchronizingSnapshot() {
+        // given
+        String expectedName = "newCatsbi";
+        TestPerson person = new TestPerson(1L, "catsbi", 55, "casbi@naver.com", 123);
+        EntityEntry entry = EntityEntry.newEntry(person, Status.MANAGED);
+
+        // when
+        person.setName(expectedName);
+        entry.synchronizingSnapshot();
+
+        Object snapshot = entry.getSnapshot();
+        Object actual = ReflectionUtils.getFieldValue(snapshot, "name");
+
+        // then
+        assertThat(actual).isEqualTo(expectedName);
+    }
+
+    @Test
+    @DisplayName("synchrnoizingSnapshot 함수는 식별자나 Transient 애노테이션 필드의 변경사항은 반영하지 않는다.")
+    void testSynchronizingSnapshotIdAndTransient () {
+        // given
+        TestPerson person = new TestPerson(1L, "catsbi", 55, "casbi@naver.com", 123);
+        EntityEntry entry = EntityEntry.newEntry(person, Status.MANAGED);
+
+        // when
+        person.setId(2L);
+        person.setIndex(456);
+        entry.synchronizingSnapshot();
+
+        Object snapshot = entry.getSnapshot();
+        Object actualId = ReflectionUtils.getFieldValue(snapshot, "id");
+        Object actualIndex = ReflectionUtils.getFieldValue(snapshot, "index");
+
+
+        // then
+        assertThat(actualId).isEqualTo(1L);
+        assertThat(actualIndex).isNull();
+    }
+
+    @Test
+    @DisplayName("isDirty 함수는 변경이 필요한 엔티티가 있을 경우 true를 반환한다.")
+    void testIsDirtyWithDirtyEntity() {
+        // given
+        TestPerson person = new TestPerson(1L, "catsbi", 55, "casbi@naver.com", 123);
+        EntityEntry entry = EntityEntry.newEntry(person, Status.MANAGED);
+
+        // when
+        person.setName("newCatsbi");
+
+        // then
+        assertThat(entry.isDirty()).isTrue();
+    }
+
+    @Test
+    @DisplayName("isDirty 함수는 변경이 필요한 엔티티가 없을 경우 false를 반환한다.")
+    void testIsDirtyWithNotDirtyEntity() {
+        // given
+        TestPerson person = new TestPerson(1L, "catsbi", 55, "casbi@naver.com", 123);
+        EntityEntry entry = EntityEntry.newEntry(person, Status.MANAGED);
+
+        // when
+        person.setIndex(456);
+
+        // then
+        assertThat(entry.isDirty()).isFalse();
+    }
+
+    @ParameterizedTest
+    @DisplayName("isDirty 함수는 entity나 snapshot이 null인 경우 true를 반환한다.")
+    @MethodSource("provideNullEntityOrSnapshotEntry")
+    void testIsDirtyWithNullEntityOrSnapshot(EntityEntry actualEntry) {
+
+        // when
+        boolean actual = actualEntry.isDirty();
+
+        // then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    @DisplayName("isDirty 함수는 entity와 snapshot이 동일할 경우 false를 반환한다.")
+    void testIsDirtyEqualsEntityAndSnapshot() {
+        // given
+        TestPerson person = new TestPerson(1L, "catsbi", 55, "casbi@naver.com", 123);
+        EntityEntry entry = EntityEntry.newEntry(person, Status.MANAGED);
+
+        // when
+        boolean actual = entry.isDirty();
+
+        // then
+        assertThat(actual).isFalse();
+
+    }
+
+
+    public static Stream<Arguments> provideNullEntityOrSnapshotEntry() {
+        EntityEntry entityNullEntry = createDummyEntry();
+        entityNullEntry.updateEntity(null);
+        EntityEntry snapshotNullEntry = createDummyEntry();
+        ReflectionUtils.setFieldValue(snapshotNullEntry, "snapshot", null);
+
+        return Stream.of(
+                Arguments.of(entityNullEntry),
+                Arguments.of(snapshotNullEntry)
+        );
+    }
+
+    private static EntityEntry createDummyEntry() {
+        TestPerson catsbi = new TestPerson(1L, "catsbi", 55, "casbi@naver.com", 123);
+        SimpleMetadataLoader<TestPerson> loader = new SimpleMetadataLoader<>(TestPerson.class);
+        Object snapshot = createSnapshot(catsbi, loader);
+
+        return new EntityEntry(loader, Status.MANAGED, catsbi, snapshot, new KeyHolder(TestPerson.class, catsbi.getId()));
+    }
+
+    private static <T> T createSnapshot(Object source, MetadataLoader<?> loader) {
+        try {
+
+            Object snapshotEntity = loader.getNoArgConstructor().newInstance();
+            for (int i = 0; i < loader.getColumnCount(); i++) {
+                Field field = loader.getField(i);
+                field.setAccessible(true);
+                field.set(snapshotEntity, field.get(source));
+            }
+
+            return (T) snapshotEntity;
+        } catch (ReflectiveOperationException e) {
+            throw new IllegalStateException("Failed to create snapshot entity");
+        }
     }
 }
--- src/test/java/persistence/util/ReflectionUtils.java
+++ src/test/java/persistence/util/ReflectionUtils.java
@@ -1,5 +1,7 @@
 package persistence.util;
 
+import persistence.sql.entity.EntityEntry;
+
 import java.lang.reflect.Field;
 
 public class ReflectionUtils {
@@ -13,6 +15,16 @@ public class ReflectionUtils {
             Field field = object.getClass().getDeclaredField(fieldName);
             field.setAccessible(true);
             return (T) field.get(object);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void setFieldValue(Object target, String fieldName, Object value) {
+        try {
+            Field field = target.getClass().getDeclaredField(fieldName);
+            field.setAccessible(true);
+            field.set(target, value);
         } catch (NoSuchFieldException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
