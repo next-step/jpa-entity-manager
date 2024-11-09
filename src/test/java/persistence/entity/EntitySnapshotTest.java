01bab1b4d0da541d7f8cc0997db4629b62e88df2
hellonayeon <hellonykwon@gmail.com>
2024-11-09 15:37:09 +0900

    test: EntitySnapshot compare() -> hasDifferenceWith() 테스트로 수정

--- src/test/java/persistence/entity/EntitySnapshotTest.java
+++ src/test/java/persistence/entity/EntitySnapshotTest.java
@@ -1,40 +1,22 @@
 package persistence.entity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.lang.reflect.Field;
-import org.jetbrains.annotations.NotNull;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class EntitySnapshotTest {
 
     @Test
-    @DisplayName("[성공] Snapshot 과 Entity 를 비교했을 때 변경된 필드만 설정된다.")
-    void compare() {
+    @DisplayName("[성공] Snapshot 과 Entity 의 차이 여부를 확인한다.")
+    void hasDifferenceWith() {
         EntityFixture entity = new EntityFixture(100, 100L, null);
         EntitySnapshot snapshot = new EntitySnapshot(entity);
 
         entity.setField1(9999);
         entity.setField3("hellonayeon");
 
-        Object diffObject = snapshot.compare(entity);
-        Assertions.assertAll("변경된 필드값 검증",
-                () -> assertEquals(getField("field1", diffObject), 9999),
-                () -> assertEquals(getField("field2", diffObject), 0L),
-                () -> assertEquals(getField("field3", diffObject), "hellonayeon"));
-    }
-
-    private Object getField(String fieldName, Object diffObject) throws IllegalAccessException, NoSuchFieldException {
-        Field field = getField(EntityFixture.class, fieldName);
-        field.setAccessible(true);
-        return field.get(diffObject);
-    }
-
-    @NotNull
-    private Field getField(Class<?> clazz, String fieldName) throws NoSuchFieldException {
-        return clazz.getDeclaredField(fieldName);
+        assertTrue(snapshot.hasDifferenceWith(entity));
     }
 
     private static class EntityFixture {
